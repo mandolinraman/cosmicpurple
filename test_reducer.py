21b5bf94ce658f2c6857d34fe5f360d840dc08b7
Raman Venkataramani <mandolinraman@gmail.com>
2022-04-12 17:25:40 -0600

    Fixes test script

--- test_reducer.py
+++ test_reducer.py
@@ -1,11 +1,13 @@
 import numpy as np
-from scipy.special import logsumexp
 from utils import *
 
+z
 
 num = 10000
 num_buckets = 5000
-buckets = np.random.permutation(np.tile(range(num_buckets), num//num_buckets))
+buckets = np.random.permutation(
+    np.tile(range(num_buckets), num // num_buckets)
+)
 # buckets = np.random.randint(0, num_buckets, num)
 
 metrics = np.random.rand(num)
@@ -19,14 +21,16 @@ multi_red = MultiReducer(num_buckets, buckets)
 locations = [np.where(buckets == i)[0] for i in range(num_buckets)]
 
 b0 = np.zeros(num_buckets)
-%timeit multi_red.reduce(metrics, b0)
-%timeit b0 = [np.max(metrics[locations[i]]) for i in range(num_buckets)]
-%timeit b0 = [np.max(metrics[j] for j in locations[i]) for i in range(num_buckets)]
-%timeit b0 = [np.max([metrics[j] for j in locations[i]]) for i in range(num_buckets)]
+multi_red.reduce(metrics, b0)
+b0 = [np.max(metrics[locations[i]]) for i in range(num_buckets)]
+b0 = [np.max(metrics[j] for j in locations[i]) for i in range(num_buckets)]
+b0 = [np.max([metrics[j] for j in locations[i]]) for i in range(num_buckets)]
 
 # soft reduce
 b1 = np.zeros(num_buckets)
-%timeit multi_red.soft_reduce(metrics, b1)
-%timeit b1 = [logsumexp(metrics[locations[i]]) for i in range(num_buckets)]
+multi_red.soft_reduce(metrics, b1)
+b1 = [logsumexp(metrics[locations[i]]) for i in range(num_buckets)]
 # %timeit b1 = [logsumexp(metrics[j] for j in locations[i]) for i in range(num_buckets)]
-%timeit b1 = [logsumexp([metrics[j] for j in locations[i]]) for i in range(num_buckets)]
+b1 = [
+    logsumexp([metrics[j] for j in locations[i]]) for i in range(num_buckets)
+]
